3c4b1d7532d98d17a469d5fdad2b756956a1dfaa
Francisco Perez <fraper02@gmail.com>
2025-08-14 01:08:55 +0200

    Actualización Autor

--- fraccionesyopp.py
+++ fraccionesyopp.py
@@ -8,7 +8,7 @@
 #   - Creación de clases y objetos
 #   - Sobrecarga de operadores para aritmética básica (+, -, *, /)
 #   - Herencia (la clase FraccionMixta hereda de Fraccion)
-#
+# Elaboración: Francisco Javier Pérez 
 
 class Fraccion:
     """Clase para representar y operar con fracciones."""
